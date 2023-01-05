90f0d494d13821704d29ff4a3e1076b1064e4feb
Suat-17 <yargicisuat@gmail.com>
2023-01-05 16:48:49 +0300

    chore: added styles

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Index = () => {
+  const [positon,setPosition] =useState({})
+  const clickHandle = (e) => {
+    // console.log(e.target.offsetWidth)
+    // console.log(e.target.getBoundingClientRect());
+    const { left, top, width, height } = e.target.gerBoundingClientRect();
+    setPosition({ left, top, width, height });
+  };
   return (
     <div>
       <nav className="menu">
-        <button>Hakkımda</button>
-        <button>İletişim</button>
-        <button>Blog</button>
-        <button>makaleler</button>
-        <button>Dersler</button>
+        <div className="divider"/>
+        <button onClick={clickHandle}>Hakkımda</button>
+        <button onClick={clickHandle}>İletişim</button>
+        <button onClick={clickHandle}>Blog</button>
+        <button onClick={clickHandle}>makaleler</button>
+        <button onClick={clickHandle}>Dersler</button>
       </nav>
     </div>
   );
