bca87c6b3d183a8729435b3293dc6c65dc644ebf
Suat-17 <yargicisuat@gmail.com>
2023-01-05 16:57:45 +0300

    chore added driver

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,27 @@
 import React, { useState } from "react";
 
 const Index = () => {
-  const [positon,setPosition] =useState({})
+  const [position,setPosition] =useState({})
   const clickHandle = (e) => {
     // console.log(e.target.offsetWidth)
     // console.log(e.target.getBoundingClientRect());
-    const { left, top, width, height } = e.target.gerBoundingClientRect();
+    const { left, top, width, height } = e.target.getBoundingClientRect();
     setPosition({ left, top, width, height });
   };
   return (
     <div>
       <nav className="menu">
-        <div className="divider"/>
+        {
+          Object.values(position).length> 0 && (<div className="divider"
+          style={{
+            '--left':position.left + 'px',
+            '--top':position.top  + 'px',
+            '--width':position.width  + 'px',
+            '--height':position.height  + 'px'
+          }}
+          />)
+        }
+        
         <button onClick={clickHandle}>Hakkımda</button>
         <button onClick={clickHandle}>İletişim</button>
         <button onClick={clickHandle}>Blog</button>
